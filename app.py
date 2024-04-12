44b76ef8c2e278b94ee1a200bd035729905f3e4c
xerneas02 <mathis.roubille@gmail.com>
2024-04-12 14:04:16 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,97 @@
+from flask import Flask, request, jsonify
+from langchain_openai import ChatOpenAI
+from langchain.schema import (
+    SystemMessage,
+    HumanMessage,
+    AIMessage,
+)
+import os
+from datetime import datetime
+import sys
+
+deploy = False
+
+if len(sys.argv) >= 2:
+    deploy = ("-d" in sys.argv or "--deploy" in sys.argv)
+
+today = datetime.today().strftime('%Y-%m-%d')
+
+if not os.path.exists("data.csv"):
+    with open("data.csv", "w") as file:
+        file.write("Question;Answer;Rating;Comment\n")
+
+app = Flask(__name__, static_folder='static')
+
+chat = ChatOpenAI(
+    openai_api_key= os.environ.get("OPENAI_API_KEY"),
+    model="gpt-3.5-turbo"
+)
+
+informations_txt = open("informations.txt", "r")
+informations = informations_txt.read()
+informations_txt.close()
+
+informations = informations.split("$$\n")
+informations.append(f"\nToday date : {today}")
+
+source_knowledge = "||".join(informations)
+
+messages = []
+prompt_answer = {"prompt":"","answer":""}
+
+@app.route("/")
+def index():
+    """Serve the main HTML file"""
+    return app.send_static_file("index.html")
+
+@app.route("/ask", methods=["POST"])
+def ask():
+    global prompt_answer
+    global messages
+    
+    query = request.json["query"]
+    prompt_answer["prompt"] = query
+
+    augmented_prompt = f"""You are a chatbot named H4nn4h (devloped by Mathis Roubille) here to assist students in obtaining information about the UiT The Arctic University of Norway in Narvik, particularly regarding the Industrial Engineering M.Sc. program in Narvik. If the query is in a different language, as there are students from abroad, try to respond in their language. If the query is completely off-topic, please indicate so, but you can answer some questions if they are about Narvik, life there, or related topics not directly connected to the master's degree. Please keep responses brief and to the point if possible and try providing links when available (the interface doesn't handle markdown).
+
+    Contexts:
+    {source_knowledge}
+
+    Query: {query}"""
+
+
+    # Create new user prompt
+    prompt = HumanMessage(content=augmented_prompt)
+    messages.append(prompt)
+
+    # Send to OpenAI
+    res = chat.invoke(messages)
+    
+    messages = []
+    messages.append(res)
+
+    prompt_answer["answer"] = res.content
+    # Return response in JSON format
+    return jsonify({"content": res.content}) 
+
+@app.route("/rate", methods=["POST"])
+def rate():
+    global prompt_answer
+    rating = request.json["rating"]
+    comment = request.json.get("comment", "")  # Get comment if provided, otherwise default to empty string
+    comment = comment.replace("\n", " ")
+    prompt_answer['prompt'] = prompt_answer['prompt'].replace(';', ',').replace('\n', ' ')
+    prompt_answer['answer'] = prompt_answer['answer'].replace(';', ',').replace('\n', ' ')
+    
+    with open("data.csv", "a") as file:
+        file.write(f"{prompt_answer['prompt']};{prompt_answer['answer']};{rating};{comment}\n")
+
+    return jsonify({"message": "Rating submitted successfully"})
+
+
+
+if __name__ == "__main__":
+    if not deploy:
+        app.run(debug=True)
+    else:
+        app.run(host='0.0.0.0', port=5000, debug=False)
---
+++ static/script.js
@@ -0,0 +1,230 @@
+async function askQuestion() {
+    const question = document.getElementById('questionInput').value;
+    const conversationContainer = document.getElementById('conversationContainer');
+    const loadingSpinner = document.getElementById('loadingSpinner');
+    const ratingContainer = document.getElementById('ratingContainer');
+    const questions = document.getElementsByClassName('question');
+
+    for (let index = 0; index < questions.length; index++) {
+        if (questions[index].tagName.toLowerCase() === 'input' || 
+            questions[index].tagName.toLowerCase() === 'button') {
+            questions[index].disabled = true;
+        }     
+    }
+
+    // Show loading spinner
+    loadingSpinner.classList.remove('hidden');
+
+    // Append user message to conversation container
+    const userMessage = createUserMessageElement(question);
+    conversationContainer.appendChild(userMessage);
+
+    // Clear question input
+    document.getElementById('questionInput').value = '';
+
+    // Scroll to the bottom of conversation container
+    window.scrollTo(0, document.body.scrollHeight);
+
+    // Create and append bot message element with typing animation
+    const botMessage = createBotTypingElement();
+    conversationContainer.appendChild(botMessage);
+
+    // Fetch bot response
+    const response = await fetch('/ask', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ query: question })
+    });
+
+    const data = await response.json();
+
+    // Hide loading spinner after receiving response
+    loadingSpinner.classList.add('hidden');
+
+    // Replace typing animation with actual bot message
+    const actualBotMessage = createBotMessageElement(data.content);
+    conversationContainer.replaceChild(actualBotMessage, botMessage);
+
+    // Scroll to the bottom of conversation container
+    window.scrollTo(0, document.body.scrollHeight);
+
+    // Show rating container
+    ratingContainer.classList.remove('hidden');
+    ratingContainer.style.display = 'flex';
+
+    // Hide question elements and enable input elements
+    for (let index = 0; index < questions.length; index++) {
+        questions[index].disabled = false;
+        questions[index].classList.add('hidden');
+    }
+}
+
+function createBotTypingElement() {
+    const botMessage = document.createElement('div');
+    botMessage.classList.add('chat-message', 'bot-message');
+
+    const typingAnimation = document.createElement('div');
+    typingAnimation.classList.add('dot-flashing');
+
+    botMessage.appendChild(typingAnimation);
+
+    return botMessage;
+}
+
+
+
+async function submitRating() {
+    const rating = document.querySelector('input[name="rating"]:checked').value;
+    const comment = document.getElementById('commentInput').value;
+    const questions = document.getElementsByClassName('question');
+
+    // Send rating and comment to server
+    const response = await fetch('/rate', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ rating: rating, comment: comment })
+    });
+
+    // Empty the comment input
+    document.getElementById('commentInput').value = '';
+
+    // Reset the rating
+    const ratingInputs = document.querySelectorAll('input[name="rating"]');
+    ratingInputs.forEach(input => {
+        input.checked = false;
+    });
+
+    // Hide rating container after submitting
+    for (let index = 0; index < questions.length; index++) {
+        if (questions[index].tagName.toLowerCase() === 'input' || 
+            questions[index].tagName.toLowerCase() === 'button') {
+            questions[index].disabled = false;
+            questions[index].classList.remove('hidden');
+        } 
+    }
+    document.getElementById('ratingContainer').classList.add('hidden');
+    ratingContainer.style.display = 'none';
+}
+
+
+// Trigger askQuestion function when Enter key is pressed
+document.getElementById('questionInput').addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+        event.preventDefault();
+        askQuestion();
+    }
+});
+
+function createUserMessageElement(message) {
+    var userMessageElement = document.createElement("div");
+    userMessageElement.classList.add("chat-message", "user-message");
+
+    var messageContent = document.createElement("div");
+    messageContent.classList.add("message-content");
+
+    var messageName = document.createElement("span");
+    messageName.classList.add("message-name");
+    messageName.textContent = "You:";
+
+    var messageText = document.createElement("span");
+    messageText.classList.add("message-text");
+    messageText.textContent = message;
+
+    messageContent.appendChild(messageName);
+    messageContent.appendChild(messageText);
+
+    userMessageElement.appendChild(messageContent);
+
+    return userMessageElement;
+}
+
+function createBotMessageElement(message) {
+    var botMessageElement = document.createElement("div");
+    botMessageElement.classList.add("chat-message", "bot-message");
+
+    var messageContent = document.createElement("div");
+    messageContent.classList.add("message-content");
+
+    var botProfilePicture = document.createElement("img");
+    botProfilePicture.src = "https://th.bing.com/th/id/OIG4.alKiiFcDb1F28PIUwDEd?w=1024&h=1024&rs=1&pid=ImgDetMain";
+    botProfilePicture.alt = "Chatbot Profile Picture";
+    botProfilePicture.classList.add("profile-picture");
+
+    var botMessageName = document.createElement("span");
+    botMessageName.classList.add("message-name");
+    botMessageName.textContent = "H4nn4h:";
+
+    // Create a span to contain the message text
+    var botMessageTextContainer = document.createElement("span");
+    botMessageTextContainer.classList.add("message-text");
+
+    // Regular expressions to match links from different domains
+    const linkRegexes = [
+        /(https?:\/\/\S*\.?uit\.no\S*)/g, // Links from uit.no
+        /(https?:\/\/\S*\.?samskipnaden\.no\S*)/g, // Links from samskipnaden.no
+        /(https?:\/\/www\.facebook\.com\/\S*)/g // Facebook links
+    ];
+
+    // Split the message into parts based on the regular expressions
+    const parts = splitMessageByRegexes(message, linkRegexes);
+
+    // Iterate through the parts
+    parts.forEach((part, index) => {
+        // Replace newline characters with <br> tags
+        if (part.includes('\n')) {
+            const lines = part.split('\n');
+            lines.forEach((line, lineIndex) => {
+                botMessageTextContainer.appendChild(document.createTextNode(line));
+                if (lineIndex < lines.length - 1) {
+                    botMessageTextContainer.appendChild(document.createElement('br'));
+                }
+            });
+        } else {
+            part = part.replace(/[\)\,:]$/, '');
+
+            if (isLink(part)) {
+                // If the part is a link, create a link element
+                var linkElement = document.createElement('a');
+                linkElement.href = part;
+                linkElement.target = '_blank';
+                linkElement.textContent = part;
+                botMessageTextContainer.appendChild(linkElement);
+            } else {
+                // If the part is not a link, create a text node
+                botMessageTextContainer.appendChild(document.createTextNode(part));
+            }
+        }
+
+        // Add a line break between parts
+        if (index < parts.length - 1) {
+            botMessageTextContainer.appendChild(document.createElement('br'));
+        }
+    });
+
+    messageContent.appendChild(botProfilePicture);
+    messageContent.appendChild(botMessageName);
+    messageContent.appendChild(botMessageTextContainer);
+
+    botMessageElement.appendChild(messageContent);
+
+    return botMessageElement;
+}
+
+
+// Function to split the message into parts based on multiple regular expressions
+function splitMessageByRegexes(message, regexes) {
+    let parts = [message];
+    regexes.forEach(regex => {
+        parts = parts.flatMap(part => part.split(regex));
+    });
+    return parts.filter(part => part !== '');
+}
+
+// Function to check if a string is a link
+function isLink(str) {
+    return /^(https?|ftp):\/\/[^\s/$.?#].[^\s]*$/.test(str);
+}
